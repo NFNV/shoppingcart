7ed607fe81eed0e5a23aa08289ac2dd15fd854cf
Nicolás Valiconto <nvaliconto@outlook.com>
2022-10-14 11:28:34 -0300

    cart, actions, reducer

---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+  return (
+    <div>
+        <h2>ShoppingCart</h2>
+        <h3>Products</h3>
+        <article className="box"></article>
+        <h3>Cart</h3>
+        <article className="box"></article>
+    </div>
+  )
+}
+
+export default ShoppingCart
